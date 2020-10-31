fde6ba34db9991b0dc75cabdcebc428a091260b1
ibkov <34207589+ibkov@users.noreply.github.com>
2020-10-31 03:52:04 +0300

    add django_site main_page.html

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Animal(models.Model):
-    name = models.CharField("Кличка", null=True, max_length=50)
+    SEX_ANIMAL = (
+        ('M', 'Кабель'),
+        ('Ж', 'Сучка'),
+    )
+
+    name = models.CharField("Кличка", null=True, max_length=50),
+    color = phone = models.CharField(max_length=30, verbose_name="Цвет животного", default=""),
+    sex = models.CharField(max_length=30, verbose_name="Пол", choices=SEX_ANIMAL, default=""),
