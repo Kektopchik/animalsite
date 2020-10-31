cc7b404388b6d1a6ecc191e9cbcbab2f62aa78af
ibkov <34207589+ibkov@users.noreply.github.com>
2020-10-31 03:39:50 +0300

    add django_site main_page.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,6 +3,6 @@ from django.views.generic import ListView
 from .models import Animal
 
 class MainView(ListView):
-    puple = Animal
+    animal = Animal
     queryset = Animal.objects.all()
     template_name = "main_page.html"
